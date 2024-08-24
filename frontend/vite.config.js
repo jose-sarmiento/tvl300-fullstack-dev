842975cecc41f792f6f2a5adb1f9e6a74e464d1c
Jose Sarmiento <josesarmiento.me@gmail.com>
2024-08-24 12:45:08 +0800

    feat(frontend): setup client to use production proxy in prod env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 5000,
-        proxy: {
-            "/api": "http://localhost:5001",
+export default defineConfig(({ mode }) => {
+    const proxyConfig =
+        mode === "production"
+            ? { "/api": process.env.PROD_API_URL } // Production proxy
+            : { "/api": "http://localhost:5001" }; // Local dev proxy
+
+    return {
+        plugins: [react()],
+        server: {
+            port: 5000,
+            proxy: proxyConfig,
         },
-    },
+    };
 });
